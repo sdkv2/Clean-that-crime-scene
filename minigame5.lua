4128437dacff5c9ed7bc6e24a05ed01cd45e5958
Ella <433585@edu.cwa.ac.uk>
2024-04-11 23:00:25 +0100

    Update Minigame5 diary entry for grandpa's death and exorcism plan

--- minigame5.lua
+++ minigame5.lua
@@ -12,8 +12,8 @@ function Minigame5.new(Parent)
 
     self.ParentMinigame = Parent
     self.pages = 
-    {"Dear diary, \n \n this is my first time writing something in 12 years but I figured it was important enough to remember. I've discovered this thing called Looksmaxxing, and holy mew! I definitely mogged Kiran today fr",
-    "Dear diary, \n \n my grandpa died today. I think he took me out of the will 'cos I was low-key mogging him so im fr gonna exorcize his ghost tonight. I'll keep you updated on how it goes. ", 
+    {"Dear diary, \n \n this is my first time writing something in 12 years but I figured it was important enough to remember. I've discovered this thing called Looksmaxxing, and holy mew! Cannot wait to unleash my sigma rizz!",
+    "Dear diary, \n \n my grandpa died today. I think he took me out of the will because I was low-key mogging him so im fr gonna exorcize his ghost tonight. I'll keep you updated on how it goes. ", 
     "*This page contains frantic scribbles detailing the exorcism process, it mentions a strange amount of dancing and arrows pointing in different directions*", 
     "Dear diary, \n \n I think I might be possessed by my grandpap's ghost. I've been seeing him in the mirror and he keeps telling me to 'get a job'. I'm not sure what that really means. BTW I also changed the PC password today for security. New code is 'password1'. ",}
     self.currentPage = 1
