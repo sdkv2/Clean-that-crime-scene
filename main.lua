89e72a41b9bfc0cc1ede8d713b05345dbcb97e3e
Ella <433585@edu.cwa.ac.uk>
2024-04-11 13:59:06 +0100

    Update minigame5.lua to improve diary entries in Minigame 5

--- main.lua
+++ main.lua
@@ -515,7 +515,7 @@ function love.keypressed(key)
 
         if key == "j" then
             fade.isActive = true
-            minigame:setMinigame(3)
+            minigame:setMinigame(5)
 
         end
         if key == "x" then
